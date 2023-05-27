c4b36778c44b65a621a95369a8ce0aebcd55a3f0
mluisas <43687500+mluisas@users.noreply.github.com>
2023-05-27 12:56:32 +0000

    altera página inicial para mostrar cantadas aleatórias

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from 'react'
+import cantadas from './cantadas.json';
+
 function Home(){
-    return <h1>Você deve ser Windows 95, porque você me deixa instável.</h1>
+    const [cantada, setCantadas] = useState(cantadas[0]);
+    useEffect(() => setCantadas(cantadaAleatoria()));
+    return (
+        <><h1>Cantadas que você merecia ouvir, mas seu crush não tem coragem de usar</h1><p>{cantada}</p><footer>Fonte: <a href='https://www.dicionariopopular.com/melhores-cantadas/'>Dicionário Popular</a></footer></>
+    );
+}
+
+function cantadaAleatoria(){
+    return cantadas[Math.floor(Math.random() * Object.keys(cantadas).length)];
 }
 
 export default Home;
